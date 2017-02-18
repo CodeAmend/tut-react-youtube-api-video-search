75be6060475b901419989f10152188aec7133c56
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-02-18 18:06:22 -0500

    Refacter to React.Component ES6 style import.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,9 @@
-import React from 'react';
+// { Compoent } <-- Create const Comonent imported from React called Component
+// same as `const Component = React.Component`
+import React, { Component } from 'react';
 
 //  We need a class-based componet. More intellegent with values and methods
-class SearchBar extends React.Component {
+class SearchBar extends Component {
   // ever class based React compoent must have render component
   render() {
     return <input />;
