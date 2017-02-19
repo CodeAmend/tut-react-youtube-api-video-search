49df5c1963694a266fea2971c6774c87d06de8fd
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-02-18 20:18:03 -0500

    created onChange event for SearchBar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -6,8 +6,13 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
   // ever class based React compoent must have render component
   render() {
-    return <input />;
+    return <input onChange={ this.onInputChange } />;
   }
+
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
 }
 
 export default SearchBar;
