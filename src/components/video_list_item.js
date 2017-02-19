86c4564584ae267d90de0ee39312427e613ca796
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-02-19 12:03:43 -0500

    updated VideoListItem with css and markup

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
-const VideoListItem = () => {
-  return <li>Video</li>
+const VideoListItem = ({video}) => {
+
+  const imageUrl = video.snippet.thumbnails.default.url;
+  const videoTitle = video.snippet.title;
+
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <div className="media-object">
+            <img src={imageUrl} />
+          </div>
+        </div>
+        <div className="media-body">
+          <div className="media-heading">
+            {videoTitle}
+          </div>
+        </div>
+      </div>
+    </li>
+  )
 }
 
 export default VideoListItem;
