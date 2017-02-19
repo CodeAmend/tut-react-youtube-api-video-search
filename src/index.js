c1190b85c6568fe6fb39cdc4c7d3bbc6c5ad9133
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-02-19 12:26:27 -0500

    VideoDetail passed to app

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
 import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyBonG_EHrxqUyl2ChI2V08goBBZL7BxCWw'
@@ -23,6 +25,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
         <VideoList videos={this.state.videos} />
       </div>)
   }
