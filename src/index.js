d8ea37e5e61c9467de4a8ee05930b8dcfc6adc4c
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-02-19 09:06:32 -0500

    moved YTSearch to constructor

--- src/index.js
+++ src/index.js
@@ -5,16 +5,25 @@ import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyBonG_EHrxqUyl2ChI2V08goBBZL7BxCWw'
-YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {
-  console.log(data);
-})
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = { videos: [] }
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    })
+
+  }
+
   render() {
     return (
       <div>
         <SearchBar />
-      </div>)    
+      </div>)
   }
 }
 
